cab328282f72cfa8f5e346b0e0b7193f070a2f2d
Tim Oerlemans <t.oerlemans@loket.nl>
2021-09-24 06:31:47 +0200

    Improvements

--- src/_utils/sort-blogposts-by-date.js
+++ src/_utils/sort-blogposts-by-date.js
@@ -1,7 +1,7 @@
-module.exports = (collection) => collection.getFilteredByGlob('./src/blog/*.md')
+module.exports = (collection) => collection.getFilteredByGlob('./src/blog/**/*.md')
         .sort((a, b) => {
             const aDate = new Date(a.date.postDate);
             const bDate = new Date(b.date.postDate);
             return +bDate - +aDate;
-        })
+        });
 
